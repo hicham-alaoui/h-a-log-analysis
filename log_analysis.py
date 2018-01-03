faaf0f39552769dda38476b56e627b1bf28c92e1
Hicham <hicham.alaoui@leedscitycollege.ac.uk>
2018-01-03 23:40:50 +0000

    Update log_analysis.py

--- log_analysis.py
+++ log_analysis.py
@@ -1,69 +1,64 @@
-# UDACITY Log Analysis Project
+#!/usr/bin/env python
 
-# import psycopg2 to implement DB-API
 import psycopg2
 
 
-# function to connect to the news database
-def connect():
-    return psycopg2.connect("dbname=news")
+def connect(dbname="news"):
+    """Connect to PostgreSQL database."""
+    try:
+        db = psycopg2.connect("dbname={}".format(dbname))
+        c = db.cursor()
+        return db, c
+    except:
+        print("Oops! error in connecting to databse.")
 
-# top three articles query
-articles = "select title, hits from top_three_articles limit 3"
 
-
-def top_articles(articles):
-    a = connect()
-    c = a.cursor()
-    c.execute(articles)
+def most_popular_articles():
+    """Query for most popular three articles"""
+    db, c = connect()
+    articles_query = "select * from top_three_articles limit 3"
+    c.execute(articles_query)
     counts = c.fetchall()
     for x in range(len(counts)):
-        title = counts[x][0]
-        hits = counts[x][1]
-        print("%s| Total hits: %d" % (title, hits))
-    a.close()
+            title = counts[x][0]
+            hits = counts[x][1]
+            print("%s| Total hits: %d" % (title, hits))
 
-# top three authors query
-authors = "select * from top_three_authors"
-
-
-def top_authors(authors):
-    b = connect()
-    c = b.cursor()
-    c.execute(authors)
+def most_popular_authors():
+    """Query for most popular authors"""
+    db, c = connect()
+    authors_query = "select * from top_three_authors"
+    c.execute(authors_query)
     counts = c.fetchall()
     for y in range(len(counts)):
         name = counts[y][0]
         hits = counts[y][1]
         print("%s| Total hits:  %d" % (name, hits))
-    b.close()
 
-# more than 1% error rate query
-
-errors = "select * from error_rate"
-
-
-def error_percent(errors):
-    d = connect()
-    c = d.cursor()
-    c.execute(errors)
+def error_percent():
+    """Query for day with error rate higher than 1%"""
+    db, c = connect()
+    errors_query = "select * from error_rate"
+    c.execute(errors_query)
     counts = c.fetchall()
     for i in range(len(counts)):
         date = counts[i][0]
         percentage = counts[i][1]
         print("%s|%.1f %%" % (date, percentage))
-    d.close()
 
-# python output statements
+
+
 
 if __name__ == "__main__":
-    print("The three articles with most hits are listed below:" + "\n")
-    top_articles(articles)
+    print("The three articles with most hits are listed below:")
+    most_popular_articles()
     print("\n")
 
-    print("The three authors with most hits are listed below:" + "\n")
-    top_articles(authors)
-    print("\n")
+    print("The three authors with most hits are listed below:")
+    most_popular_authors()
+    print("\n")  
 
-    print("The day when the error rate was more than 1 percent:" + "\n")
-    error_percent(errors)
+    print("The day when the error rate was more than 1 percent:")
+    error_percent()
+
+ print "\nAll done!\n"
